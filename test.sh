1e3a18817243d243b4d1c809c7cc5841f57ae0d4
daiphuoc511 <duongdaiphuoc511@gmail.com>
2024-09-25 00:13:54 +0700

    Re-add ignored files on c

--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@ branches=$(git branch -r | grep -v '\->' | sed 's/origin\///')
 
 for branch in $branches; do
   git checkout $branch
+  rm b.text
   git rm -r --cached .
   git add .
   git commit -m "Re-add ignored files on $branch"
