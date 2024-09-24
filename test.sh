1d9475a333093f280f31c1d2a9a5d6f8464bfd36
daiphuoc511 <duongdaiphuoc511@gmail.com>
2024-09-25 00:04:01 +0700

    Re-add ignored files on c

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ branches=$(git branch -r | grep -v '\->' | sed 's/origin\///')
 
 for branch in $branches; do
   git checkout $branch
-  git add -f .gitignore test.sh
+  git add .gitignore test.sh b.text
   git commit -m "Re-add ignored files on $branch"
   git push origin $branch
 done
