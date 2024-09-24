7d0b79ff18b14c01609afa5280c3d9224d97cf6d
daiphuoc511 <duongdaiphuoc511@gmail.com>
2024-09-24 22:31:26 +0700

    cyan

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.red),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -68,7 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add, color:  Colors.red,),
+        child: const Icon(Icons.add, color:  Colors.cyan,),
       ),
     );
   }
